89398696487390104bb122f3ae96e1b1a400df29
Karanum <hyougakazu@gmail.com>
2021-05-04 19:38:57 +0200

    Update version number to 1.0.3

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const config = require('./modules/config.js');
 const app = express();
 const port = config.get("port");
 
-const versionSuffix = "1_0_2beta";
+const versionSuffix = "1_0_3beta";
 
 console.log("Starting...");
 const randomiser = require('./randomiser/randomiser.js');
