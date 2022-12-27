72288d9c782ac803a322d113c1467a19930d4b39
Karanum <mkok.personal@gmail.com>
2022-12-27 12:56:33 +0100

    Add comments to the map code system

--- randomiser/game_logic/map_code.js
+++ randomiser/game_logic/map_code.js
@@ -4,19 +4,31 @@ const decompr = require('../../modules/decompression.js');
 var mapCodeData = {};
 var preCompressed = {};
 
+/**
+ * Reads 16 bits of data from the specified position.
+ */
 function read16b(data, pos) {
     return data[pos] + (data[pos + 1] << 8);
 }
 
+/**
+ * Reads 24 bits of data from the specified position.
+ */
 function read24b(data, pos) {
     return data[pos] + (data[pos + 1] << 8) + (data[pos + 2] << 16);
 }
 
+/**
+ * Writes 16 bits of data to the specified position.
+ */
 function write16b(data, pos, write) {
     data[pos] = write & 0xFF;
     data[pos + 1] = (write >> 8) & 0xFF;
 }
 
+/**
+ * Writes 32 bits of data to the specified position.
+ */
 function write32b(data, pos, write) {
     data[pos] = write & 0xFF;
     data[pos + 1] = (write >> 8) & 0xFF;
@@ -24,6 +36,11 @@ function write32b(data, pos, write) {
     data[pos + 3] = (write >> 24) & 0xFF;
 }
 
+/**
+ * Converts BL calls
+ * @param {byte[]} data The map code.
+ * @param {boolean} restore The conversion direction (true for compression, false for decompression)
+ */
 function convertBranchLinks(data, restore) {
     var i = 2;
     while (i < data.length) {
@@ -44,44 +61,82 @@ function convertBranchLinks(data, restore) {
     }
 }
 
+/**
+ * Initialises the map code data.
+ * @param {Uint8Array} rom The ROM array to extract from
+ */
 function initialise(rom) {
     if (!fs.existsSync('./map_code_cache/')) {
         fs.mkdirSync('./map_code_cache/');
     }
 
     for (var i = 1609; i < 1723; ++i) {
+        //Get map code pointer from the master file table
         var pointer = read24b(rom, 0x680000 + 4 * i);
 
+        //Decompress map code file
         var mapCode = decompr.decompress(rom, pointer, 0);
         if (!mapCode) continue;
 
+        //Applying fixes because of old randomiser hackiness
+        //NOTE: Not sufficient to solve the Piers issue in Kibombo because this means
+        //      it can be difficult to take him out of town under certain conditions
+        if (i == 1648) {
+            mapCode[0x1224] = 0x0;
+        }
+
+        //Store map code in pre-compressed cache
+        //Reads from the map_code_cache folder if it exists to speed up initialisation
         if (fs.existsSync(`./map_code_cache/${i}.bin`)) {
             preCompressed[i.toString()] = fs.readFileSync(`./map_code_cache/${i}.bin`);
         } else {
             compressCacheMapCode(i, mapCode);
         }
         
+        //Convert BL calls and store map code
         convertBranchLinks(mapCode, false);
         mapCodeData[i.toString()] = [false, mapCode];
     }
 }
 
+/**
+ * Compresses the map code and writes the result to the map_code_cache folder.
+ * @param {number} i The MFT index
+ * @param {byte[]} mapCode The map code
+ */
 function compressCacheMapCode(i, mapCode) {
     preCompressed[i.toString()] = new Uint8Array(decompr.compressC1(mapCode));
     fs.writeFileSync(`./map_code_cache/${i}.bin`, preCompressed[i.toString()]);
 }
 
+/**
+ * Clones the map code data for safe modification.
+ * 
+ * The data is formatted as an object with the MFT indices as keys.
+ * Each key maps to a `[boolean, byte[]]` struct where the first element is whether the map code has been modified,
+ * and the second element is the binary map code data.
+ * @returns {object} A deep copy of the map code data
+ */
 function clone() {
     return JSON.parse(JSON.stringify(mapCodeData));
 }
 
+/**
+ * Writes the map code data to a ROM array.
+ * @param {{}} instance The map code data instance (see `clone`)
+ * @param {Uint8Array} rom The ROM array to write into
+ */
 function writeToRom(instance, rom) {
+    //Get the starting position for the map code data
     var romPos = read24b(rom, 0x681924);
 
     for (var i = 1609; i < 1723; ++i) {
+        //Store the new pointer in the master file table
         var pointerAddr = 0x680000 + 4 * i;
         write32b(rom, pointerAddr, romPos + 0x08000000);
 
+        //If the map code changed, compress and convert BLs
+        //Otherwise, get the pre-compressed map code from cache
         var entry = instance[i];
         var cmc = undefined;
         if (entry[0]) {
@@ -91,14 +146,19 @@ function writeToRom(instance, rom) {
             cmc = preCompressed[i];
         }
 
+        //Write the compressed map code into the ROM
         rom[romPos++] = 0x1;
         for (let i = 0; i < cmc.length; ++i) {
             rom[romPos++] = cmc[i];
         }
+
+        //Add proper data spacing
         romPos += 3;
         romPos &= 0xFFFFFFFC;
     }
 
+    //Throw a warning if the total map code data exceeds a safe size
+    //(The randomiser has data at 0xFA0000 that mustn't be overwritten)
     if (romPos >= 0xF9FF00) {
         console.log("WARNING: The size of the map code data exceeds safe limits");
     }
--- randomiser/patches/tutorial_npcs.js
+++ randomiser/patches/tutorial_npcs.js
@@ -1,11 +1,20 @@
 const textutil = require('./../game_logic/textutil.js');
 
+/**
+ * Applies this patch to the map code
+ * @param {{}} mapCode The map code data instance
+ * @param {{}} text The text data instance
+ * @param {{}} settings The settings object
+ */
 function apply(mapCode, text, settings) {
     mapCode[1616][0] = true;
 
+    //Add NPCs for general info and Retreat info
     setNpc(mapCode[1616][1], 0x3844, 0x391C, 8, 0x9D, 0xFFFF, 1, 0x190, 0, 0x2A8, 4, 1, 0x1570);
     setNpc(mapCode[1616][1], 0x385C, 0x3934, 9, 0xDC, 0xFFFF, 1, 0x160, 0, 0x278, 4, 1, 0x1572);
 
+    //Add NPCs for Avoid info and the hint system if these are enabled in the settings
+    //If both settings are enabled, an extra row needs to be added to the event table
     var npcAddr = 0x3874, eventAddr = 0x394C, npcId = 10;
     if (settings['avoid-patch']) {
         setNpc(mapCode[1616][1], npcAddr, eventAddr, npcId++, 0x83, 0xFFFF, 1, 0x120, 0, 0x280, 4, 1, 0x1574);
@@ -18,6 +27,8 @@ function apply(mapCode, text, settings) {
         setNpc(mapCode[1616][1], npcAddr, eventAddr, npcId++, 0x79, 0xFFFF, 1, 0x100, 0, 0x280, 4, 1, 0x1576);
     }
 
+    //Add lines for the new NPCs to the text data
+    //These lines overwrite part of the Venus Lighthouse opening cutscene
     textutil.writeLine(text, 0x1570, "Thanks for playing this\x03randomiser! I'm Karanum,\x03the developer.\x01If you're new, make sure\x03to check the help pages\x03on the website.\x01Also, if you get stuck, holding\x03L and Start while selecting which\x03save file to load can help.\x02");
     textutil.writeLine(text, 0x1571, "Hey now,\x03no peeking!\x01You'll get to see what I'm working\x03on when it's ready!\x02");
     textutil.writeLine(text, 0x1572, "Did you know Retreat has\x03been changed big time in\x03this randomiser?\x01Using it on the world map lets\x03you teleport to any location\x03you've visited.\x01You can even use it in towns\x03now so you can get around faster!\x02");
@@ -28,6 +39,22 @@ function apply(mapCode, text, settings) {
     textutil.writeLine(text, 0x1577, "I shall reign supreme.\x02");
 }
 
+/**
+ * Adds a new NPC to the map code tables
+ * @param {byte[]} mapCode The binary map code data
+ * @param {number} npcOffset The address to insert the NPC data into (18 bytes)
+ * @param {number} eventOffset The address to insert the talk event data into (18 bytes)
+ * @param {number} eventId The ID this NPC will be assigned, starts at 0x8 and increments for each non-party NPC in the table
+ * @param {number} sprite Sprite ID
+ * @param {number} flag The conditional flag for this NPC, `0xFFFF` for no condition, `0x1000 | flag` for inverted condition
+ * @param {number} idleScript The idle script for this NPC, either a low number for presets, or high numbers for custom behaviour
+ * @param {number} x X coordinate
+ * @param {number} z Z coordinate
+ * @param {number} y Y coordinate
+ * @param {number} direction Facing direction, 0 = east, 4 = south, 8 = west, 12 = north
+ * @param {number} flags Additional behaviour flags
+ * @param {number} dialogue Text index for the talk event, +1 for the Mind Read event
+ */
 function setNpc(mapCode, npcOffset, eventOffset, eventId, sprite, flag, idleScript, x, z, y, direction, flags, dialogue) {
     offset = npcOffset;
     mapCode[offset++] = sprite & 0xFF;
