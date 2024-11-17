8aadadc8d446bb575493cf815016deabbe80b9dc
Karanum <mkok.personal@gmail.com>
2024-11-17 18:47:49 +0100

    Remove Archipelago generation page

--- routers/public.js
+++ routers/public.js
@@ -94,8 +94,8 @@ router.get(['/permalink/:id', '/:lang/permalink/:id'], (req, res) => {
     });
 });
 
-router.get(['/archipelago/generate.html', '/:lang/archipelago/generate.html', '/archipelago', '/:lang/archipelago'], (_, res) => {
-    res.render('ap_generate.ejs');
-});
+// router.get(['/archipelago/generate.html', '/:lang/archipelago/generate.html', '/archipelago', '/:lang/archipelago'], (_, res) => {
+//     res.render('ap_generate.ejs');
+// });
 
 module.exports = router;
