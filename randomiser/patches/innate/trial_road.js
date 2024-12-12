63ac77e24903f9cbc4ac502effa31ddf42840bf8
Karanum <mkok.personal@gmail.com>
2024-12-12 15:33:02 +0100

    Change Trial Road to allow forfeiting post-Moapa

--- randomiser/patches/innate/trial_road.js
+++ randomiser/patches/innate/trial_road.js
@@ -3,16 +3,18 @@
  * to prevent duplication issues and potential item loss.
  */
 
+const { write16b } = require('./../../../util/binary');
+
 function apply(rom) {
     // Disables inventory snapshotting
-    rom[0xB10A4] = 0x8C;
-    rom[0xB10A5] = 0xE0;
+    write16b(rom, 0xB10A4, 0xE08C);
+
+    // Stop the chest flags in Trial Road from being reset
+    write16b(rom, 0xB11D0, 0xE009);
 
     // Disables restoration of inventory snapshot
-    rom[0xB125A] = 0x0;
-    rom[0xB125B] = 0x0;
-    rom[0xB1264] = 0x0;
-    rom[0xB1265] = 0x0;
+    write16b(rom, 0xB125A, 0x0);
+    write16b(rom, 0xB1264, 0x0);
 }
 
 module.exports = {apply};
